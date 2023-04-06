bfd7a7822388dda1c4ee5054708ecd05430cf550
xltvy <68481602+xltvy@users.noreply.github.com>
2023-04-06 14:42:57 +0300

    v0.2

--- wino/src/components/TopBar.js
+++ wino/src/components/TopBar.js
@@ -116,7 +116,7 @@ const TopBar = ({onInformationClick, onMobileInformationClick, onSearchClick, on
                 </div>
                 <div className='top-bar-animation-button-container'>
                     {!isMobile && (<Switch type={"button"} checked={isAnimationChecked} onChange={handleOnDesktopFilmToggle} handleColor="white" offColor="#C6CED0" checkedIcon={false} uncheckedIcon={false} height={18} width={30} onColor={"#4cd964"}/>)}
-                    {isMobile && (<Switch type={"button"} checked={isAnimationChecked} onChange={handleOnDesktopFilmToggle} handleColor="white" offColor="#C6CED0" checkedIcon={false} uncheckedIcon={false} height={24} width={40} onColor={"#4cd964"}/>)}
+                    {isMobile && (<Switch type={"button"} checked={isAnimationChecked} onChange={handleOnDesktopFilmToggle} handleColor="white" offColor="#C6CED0" checkedIcon={false} uncheckedIcon={false} height={24} width={42} onColor={"#4cd964"}/>)}
                 </div>
                 <div className="top-bar-utility-button" onClick={handleMobileDropdown}>
                     <UtilityDots className='top-bar-utility-dots'/>
--- wino/src/components/info_texts/NikeContent.js
+++ wino/src/components/info_texts/NikeContent.js
@@ -4,7 +4,7 @@ import Logo from "../../images/client_logos/nike-logo.png";
 const NikeContent = () => {
     return (
         <>
-            <img src={Logo} alt="Nike Logo" style={{ width: "60%", height: "auto", paddingTop: "8px" }} />
+            <img src={Logo} alt="Nike Logo" style={{ width: "100%", height: "auto", paddingTop: "8px" }} />
             <p>As children growing up in a culture where the street is the home of all our memories and dance is a part of every celebration, yet we went to the places we needed to reach and always come back again to our community, to the neighborhood with the Air Force 1.</p>
             <p style={{ paddingTop: "8px" }}>Here is Gen Z, the newest generation of those who feel timeless (1A). Unique working rules, liberating perception of beauty, completely different ways of self-expression. They know a place beyond what is right or wrong, what is desired or boring. We will meet them there, together with Elements of Dance (EoD) & other communities.</p>
         </>
