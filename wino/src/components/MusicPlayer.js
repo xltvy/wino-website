071d20c965dd8825118850ca54eab3ceab3a32cb
xltvy <68481602+xltvy@users.noreply.github.com>
2023-05-29 13:49:04 +0300

    fully functioning player without styles added

--- wino/src/components/MusicPlayer.js
+++ wino/src/components/MusicPlayer.js
@@ -5,6 +5,7 @@ import './component_styles.css';
 const MusicPlayer = () => {
   const [playing, setPlaying] = useState(false);
   const [currentTrackIndex, setCurrentTrackIndex] = useState(0);
+  const [played, setPlayed] = useState(0);
   const playerRef = useRef(null);
 
   const urlList = [
@@ -12,6 +13,22 @@ const MusicPlayer = () => {
     'https://www.youtube.com/watch?v=uoT2Ut8WoG4'
     // add more URLs here
   ];
+
+  const imgList = [
+    'https://i.ytimg.com/vi/wMSUZhsmttA/maxresdefault.jpg',
+    'https://i.ytimg.com/vi/uoT2Ut8WoG4/maxresdefault.jpg'
+    // add more URLs here
+    ];
+
+    const titleList = [
+        'Palace',
+        'Hun43rd'
+    ];
+
+    const artistList = [
+        'A$AP Rocky',
+        'A$AP Rocky'
+    ];
 
   const handlePlayPause = () => {
     setPlaying(!playing);
@@ -31,18 +48,38 @@ const MusicPlayer = () => {
     }
   };
 
+  const handleProgress = (progress) => {  // Add this function
+    setPlayed(progress.played);
+  };
+
   return (
     <div className='music-player-container'>
       <div className='music-player-wrapper'>
-        <div className='yt-player-container'>
+        <div className='yt-thumbnail-container'>
+            <img src={imgList[currentTrackIndex]} alt='thumbnail' />
+        </div>
+        <div>
+            <h2>{titleList[currentTrackIndex]}</h2>
+            <h3>{artistList[currentTrackIndex]}</h3>
+        </div>
+        <div>
           <ReactPlayer
             ref={playerRef}
             url={urlList[currentTrackIndex]}
-            width="100%"
-            height="100%"
+            width="0px"
+            height="0px"
             playing={playing}
+            onProgress={handleProgress}  // Add this prop
           />
         </div>
+        <input 
+          type="range" 
+          min={0} 
+          max={1} 
+          step='any' 
+          value={played} 
+          onChange={e => setPlayed(+e.target.value)}
+        />
         <button onClick={handlePlayPause}>{playing ? 'Pause' : 'Play'}</button>
         <button onClick={handlePrevTrack}>Previous</button>
         <button onClick={handleNextTrack}>Next</button>
